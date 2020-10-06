332aa4e7f62c161a08a38c58215a54e890df77ed
Daniel Wallace <danielwallace@gtmanfred.com>
2020-10-06 10:47:10 -0500

    fix(ci): add github action tests
    This is pr will add multiple test actions for the sadevbot plugins
    
    The following workflows are added
    
    - tests.yml: This runs the tests using pytest and reports code coverage
    - syntax.yml: This makes sure the black syntax formatter and flake8 both pass
    - commit.yml: This lints the commit messages for [Conventional Commits](https://www.conventionalcommits.org/en/v1.0.0/#summary)

--- tests/test_local_webserver.py
+++ tests/test_local_webserver.py
@@ -5,24 +5,25 @@ import os
 import pytest
 import requests
 import socket
-from errbot.backends.test import FullStackTest, testbot
 from time import sleep
 
 extra_plugin_dir = "."
 
 log = logging.getLogger(__name__)
 
-PYTHONOBJECT = ['foo', {'bar': ('baz', None, 1.0, 2)}]
+PYTHONOBJECT = ["foo", {"bar": ("baz", None, 1.0, 2)}]
 JSONOBJECT = json.dumps(PYTHONOBJECT)
 # Webserver port is picked based on the process ID so that when tests
 # are run in parallel with pytest-xdist, each process runs the server
 # on a different port
-WEBSERVER_PORT = int(os.environ.get("WEBSERVER_HTTP_PORT", 3142)) + \
-                 int(os.environ.get("PYTEST_XDIST_WORKER", 'gw0').replace('gw', ''))
+WEBSERVER_PORT = int(os.environ.get("WEBSERVER_HTTP_PORT", 3142)) + int(
+    os.environ.get("PYTEST_XDIST_WORKER", "gw0").replace("gw", "")
+)
 
-@pytest.fixture(scope='session', autouse=True)
+
+@pytest.fixture(scope="session", autouse=True)
 def setup_webserverport_env_var(worker_id):
-    os.environ['WEBSERVER_HTTP_PORT'] = str(WEBSERVER_PORT)
+    os.environ["WEBSERVER_HTTP_PORT"] = str(WEBSERVER_PORT)
 
 
 def webserver_ready(host, port):
@@ -35,26 +36,32 @@ def webserver_ready(host, port):
     except Exception:
         return False
 
+
 def wait_for_server(port: int):
     failure_count = 10
-    while not webserver_ready('localhost', port):
+    while not webserver_ready("localhost", port):
         waiting_time = 1.0 / failure_count
-        log.info('Webserver not ready yet, sleeping for %f second.', waiting_time)
+        log.info("Webserver not ready yet, sleeping for %f second.", waiting_time)
         sleep(waiting_time)
         failure_count -= 1
         if failure_count == 0:
             raise TimeoutError("Could not start the internal Webserver to test.")
+
 
 @pytest.fixture
 def webhook_testbot(request, testbot):
     wait_for_server(WEBSERVER_PORT)
     return testbot
 
+
 def test_not_configured_url_returns_404(webhook_testbot):
-    assert requests.post(
-        'http://localhost:{}/randomness_blah'.format(WEBSERVER_PORT),
-        "{'toto': 'titui'}"
-    ).status_code == 404
+    assert (
+        requests.post(
+            "http://localhost:{}/randomness_blah".format(WEBSERVER_PORT),
+            "{'toto': 'titui'}",
+        ).status_code
+        == 404
+    )
 
 
 def test_webserver_plugin_ok(webhook_testbot):
@@ -63,15 +70,13 @@ def test_webserver_plugin_ok(webhook_testbot):
 
 def test_trailing_no_slash_ok(webhook_testbot):
     assert requests.post(
-        'http://localhost:{}/echo'.format(WEBSERVER_PORT),
-        JSONOBJECT
+        "http://localhost:{}/echo".format(WEBSERVER_PORT), JSONOBJECT
     ).text == repr(json.loads(JSONOBJECT))
 
 
 def test_trailing_slash_also_ok(webhook_testbot):
     assert requests.post(
-        'http://localhost:{}/echo/'.format(WEBSERVER_PORT),
-        JSONOBJECT
+        "http://localhost:{}/echo/".format(WEBSERVER_PORT), JSONOBJECT
     ).text == repr(json.loads(JSONOBJECT))
 
 
